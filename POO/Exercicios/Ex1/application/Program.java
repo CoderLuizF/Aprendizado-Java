58fe27fa601ca71f1cde71d610a7edc2285eae01
Luiz Felipe <zzzezizzz@gmail.com>
2026-05-30 11:35:15 -0300

    Entrada de dados e instância de construtores contendo sobrecarga

--- POO/Exercicios/Ex1/application/Program.java
+++ POO/Exercicios/Ex1/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Account;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,6 +11,26 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        Account account;
+
+        System.out.println("Enter account number: ");
+        int number = sc.nextInt();
+
+        System.out.println("Enter account holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+
+        System.out.println("Is there an initial deposit (y/n)? ");
+        char response = sc.next().charAt(0);
+        if(response == 'y'){
+            System.out.println("Enter initial deposit value: ");
+            double initialDeposit = sc.nextDouble();
+            account = new Account(number, holder, initialDeposit);
+        }
+        else{
+            account = new Account(number, holder);
+        }
 
 
 
