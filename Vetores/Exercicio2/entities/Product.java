543644065a8058c5a3cc0bfd057fd3685cddb249
Luiz Felipe <zzzezizzz@gmail.com>
2026-05-31 15:36:16 -0300

    Métodos getters e setters criados

--- Vetores/Exercicio2/entities/Product.java
+++ Vetores/Exercicio2/entities/Product.java
@@ -10,4 +10,18 @@ public class Product {
         this.price = price;
     }
 
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+
 }
