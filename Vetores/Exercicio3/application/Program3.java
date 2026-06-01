e80d0a87cb3d45d21d8be161b8f5180e7b6a1ffb
Luiz Felipe <zzzezizzz@gmail.com>
2026-06-01 08:10:28 -0300

    1- Receber a quantidade de números que o usuário vai digitar; 2- Armazenar no vetor; 3- Percorrer o vetor e selecionar o que vai ser mostrado na tela.

--- Vetores/Exercicio3/application/Program3.java
+++ Vetores/Exercicio3/application/Program3.java
@@ -10,5 +10,24 @@ public class Program3 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+
+        System.out.println("Quantos números você vai digitar? ");
+        int n = sc.nextInt();
+
+        int[] vect = new int[n];
+
+        for(int i=0; i<vect.length; i++) {
+            System.out.println("Digite um núemro: ");
+            vect[i] = sc.nextInt();
+        }
+
+        System.out.println("Números negativos: ");
+        for(int i=0; i<vect.length; i++) {
+            if(vect[i] < 0) {
+                System.out.println(vect[i]);
+            }
+        }
+
+        sc.close();
     }
 }
