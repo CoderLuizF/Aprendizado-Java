469ca4b66dd242e912f59cf5f6d7ac3ed83cd85d
Luiz Felipe <zzzezizzz@gmail.com>
2026-06-01 08:32:10 -0300

    Criação do array para guardar "pessoas" e for para ler os dados

--- Vetores/Exercicio4/application/Program4.java
+++ Vetores/Exercicio4/application/Program4.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Pessoas;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,7 +12,29 @@ public class Program4 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Quantas pessoas serão digitadas? ");
+        int n = sc.nextInt();
 
+        //array para guardar "Pessoas"
+        Pessoas[] vect = new Pessoas[n];
+
+        //ler os dados
+        for(int i=0; i< vect.length; i++) {
+            System.out.println("Dados da " + (i + 1) + "a pessoa: ");
+
+            sc.nextLine();
+
+            System.out.println("Nome: ");
+            String nome = sc.nextLine();
+
+            System.out.println("Idade: ");
+            int idade = sc.nextInt();
+
+            System.out.println("Altura: ");
+            double altura = sc.nextDouble();
+
+            vect[i] = new Pessoas(nome, idade, altura);
+        }
 
 
 
