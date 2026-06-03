05d02870e8b14ae61d3110095a62fe4a881f022a
Luiz Felipe <zzzezizzz@gmail.com>
2026-06-03 11:48:21 -0300

    Leitura de dados do usuário

--- Vetores/Exercicio5/application/Program5.java
+++ Vetores/Exercicio5/application/Program5.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Rent;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -14,6 +16,17 @@ public class Program5 {
 
         System.out.println("How many rooms will be rented? ");
         int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            System.out.println("Rent #" + i + ": ");
+            System.out.println("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.println("Email: ");
+            String email = sc.next();
+            System.out.println("Room: ");
+
+        }
 
         sc.close();
     }
