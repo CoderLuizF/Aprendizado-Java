c4e55da3adfa768767bd01da00ef93b1b758c5b7
Luiz Felipe <zzzezizzz@gmail.com>
2026-06-11 11:56:16 -0300

    usando o .length

--- Matrizes/Exercício1/application/Program7.java
+++ Matrizes/Exercício1/application/Program7.java
@@ -15,23 +15,23 @@ public class Program7 {
         int[][] mat = new int[n][n];
 
         //ler a matriz com um 'for' dentro de outro
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++){
+        for(int i=0; i<mat.length; i++) {
+            for(int j=0; j<mat.length; j++){
                 mat[i][j] = sc.nextInt();
             }
         }
 
         //mostrar a diagonal principal
         System.out.println("Main diagonal: ");
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<mat.length; i++) {
             System.out.print(mat[i][i] + " ");
         }
         System.out.println();
 
         //exibir a quantidade de números negativos (caso tenha)
         int count = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
+        for(int i=0; i<mat.length; i++) {
+            for(int j=0; j<mat.length; j++) {
                 if(mat[i][j] < 0) {
                     count++;
                 }
