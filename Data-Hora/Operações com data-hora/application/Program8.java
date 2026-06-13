aecf6886261397d44b76181a55be2eb6fc32e3dc
Luiz Felipe <zzzezizzz@gmail.com>
2026-06-13 07:54:16 -0300

    Local date com / sem horário e com fuso horário;

--- Data-Hora/Operações com data-hora/application/Program8.java
+++ Data-Hora/Operações com data-hora/application/Program8.java
@@ -1,9 +1,20 @@
 package application;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Program8 {
 
     public static void main(String[] args) {
 
+        LocalDate d01 = LocalDate.now();
+        System.out.println("Data local atual: " + d01);
 
+        LocalDateTime d02 = LocalDateTime.now();
+        System.out.println("Data local com horário: " + d02);
+
+        Instant d03 = Instant.now();
+        System.out.printf("Data local com fuso horário: " + d03);
     }
 }
