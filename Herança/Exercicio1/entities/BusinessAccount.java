84f79b6554b3d8958f17c0c4b893ce4f44721fb0
Luiz Felipe <zzzezizzz@gmail.com>
2026-07-07 09:05:56 -0300

    Colocando o atributo loanLimit dentro do construtor

--- Herança/Exercicio1/entities/BusinessAccount.java
+++ Herança/Exercicio1/entities/BusinessAccount.java
@@ -7,7 +7,8 @@ public class BusinessAccount extends Account1 {
     public BusinessAccount(){
         super();
     }
-    public BusinessAccount(Integer number, String holder, Double balance){
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit){
         super(number, holder, balance);
+        this.loanLimit = loanLimit;
     }
 }
