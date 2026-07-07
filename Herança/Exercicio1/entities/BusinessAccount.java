2eb62cfab3fe0a61e6d587414e6013a074e6fcc5
Luiz Felipe <zzzezizzz@gmail.com>
2026-07-07 08:58:53 -0300

    Contrutor para subclasse

---
+++ Herança/Exercicio1/entities/BusinessAccount.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class BusinessAccount extends Account {
+
+
+
+    public BusinessAccount(Integer number, String holder, Double balance){
+        super(number, holder, balance);
+    }
+}
