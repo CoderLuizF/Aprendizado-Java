9c771e550525eb4f70475989195802fc6af78d8b
Luiz Felipe <zzzezizzz@gmail.com>
2026-07-07 09:32:16 -0300

    Exemplo de UPCASTING

--- Herança/Exercicio1/Program8.java
+++ Herança/Exercicio1/Program8.java
@@ -1,7 +1,16 @@
+import entities.Account;
+import entities.Account1;
+import entities.BusinessAccount;
+
 public class Program8 {
 
     public static void main(String[] args) {
 
+        Account acc = new Account1(1001, "Luiz", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Cássia", 0.0, 500.0);
+
+        //UPCASTING -> Pegar obj do BusinessAccount para o Account
+        Account1 acc1 = bacc;
 
     }
 }
