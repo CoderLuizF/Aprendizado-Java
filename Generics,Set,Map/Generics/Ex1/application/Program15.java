013fbe30ba2dce84495cb67e5facf0cc6491224f
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-03 08:10:31 -0300

    Lógica do Program pronta

--- Generics,Set,Map/Generics/Ex1/application/Program15.java
+++ Generics,Set,Map/Generics/Ex1/application/Program15.java
@@ -12,6 +12,16 @@ public class Program15 {
 
         PrintService ps = new PrintService();
 
+        System.out.println("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
 
         sc.close();
     }
