e4e821fde846db5e96431a60e60b7df4781e58e9
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-03 08:01:39 -0300

    Instanciei a classe de PrintService no program

---
+++ Generics,Set,Map/Generics/Ex1/application/Program15.java
@@ -0,0 +1,18 @@
+package application;
+
+import services.PrintService;
+
+import java.util.Scanner;
+
+public class Program15 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        PrintService ps = new PrintService();
+
+
+        sc.close();
+    }
+}
