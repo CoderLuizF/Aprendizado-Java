b89ea748e169362a8da93f9dc3f4278d33548345
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-03 08:27:01 -0300

    Utilizando o Generics para parametrizar a minha classe

--- Generics,Set,Map/Generics/Ex1/application/Program15.java
+++ Generics,Set,Map/Generics/Ex1/application/Program15.java
@@ -10,7 +10,7 @@ public class Program15 {
 
         Scanner sc = new Scanner(System.in);
 
-        PrintService ps = new PrintService();
+        PrintService<Integer> ps = new PrintService<>();
 
         System.out.print("How many values? ");
         int n = sc.nextInt();
