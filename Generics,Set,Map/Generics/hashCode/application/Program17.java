32c8bad58f6bb0ebab3f9e4495742845b9458a29
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-15 09:17:05 -0300

    hashCode diferente = objetos diferentes. Porém pode ter colisões (obj diferentes com mesmo hashCode!!)

---
+++ Generics,Set,Map/Generics/hashCode/application/Program17.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Program17 {
+
+    public static void main(String[] args) {
+
+        String a = "Luiz";
+        String b = "Cássia";
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+
+    }
+}
