f71cdb33ffc367a8128ce046d55006a62a15b385
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-15 08:53:30 -0300

    Equals: Se for igual, retorna true, se for diferente, retorna false.

--- Generics,Set,Map/Generics/Equals/application/Program16.java
+++ Generics,Set,Map/Generics/Equals/application/Program16.java
@@ -7,7 +7,13 @@ public class Program16 {
 
     public static void main(String[] args) {
 
+        String a = "Luiz";
+        String b = "Cássia";
+        String c = "Luiz";
 
+        System.out.println(a.equals(b));
+
+        System.out.println(a.equals(c));
 
     }
 }
