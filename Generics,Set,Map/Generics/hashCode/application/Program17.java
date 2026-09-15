a1b13c74b9fac31a94ce73f3530899fbf9df4875
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-15 09:58:30 -0300

    Teste de comparação.

--- Generics,Set,Map/Generics/hashCode/application/Program17.java
+++ Generics,Set,Map/Generics/hashCode/application/Program17.java
@@ -1,14 +1,21 @@
 package application;
 
+import entities.Client;
+
 public class Program17 {
 
     public static void main(String[] args) {
 
-        String a = "Luiz";
-        String b = "Cássia";
+        Client c1 = new Client("Luiz", "luiz@gmail.com");
+        Client c2 = new Client("Cássia", "cassia@gmail.com");
+        Client c3 = new Client("Cássia", "cassia@gmail.com");
 
-        System.out.println(a.hashCode());
-        System.out.println(b.hashCode());
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c3.hashCode());
+
+        System.out.println(c1.equals(c2));
+        System.out.println(c2.equals(c3));
 
     }
 }
