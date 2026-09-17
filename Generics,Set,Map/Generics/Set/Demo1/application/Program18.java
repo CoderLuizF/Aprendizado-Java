e7eed56d7be8174fe7686eb4478b6d57a82aa4c1
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-17 10:22:12 -0300

    União de coleções (conjuntos)

--- Generics,Set,Map/Generics/Set/Demo1/application/Program18.java
+++ Generics,Set,Map/Generics/Set/Demo1/application/Program18.java
@@ -1,29 +1,39 @@
 package application;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program18 {
 
     public static void main(String[] args) {
 
-        Set<String> set = new HashSet<>();
+//        Set<String> set = new HashSet<>();
+//
+//        set.add("Tv");
+//        set.add("Notebook");
+//        set.add("Tablet");
+//        set.add("Sofá");
+//        set.add("Cd");
+//
+//        System.out.println(set.contains("Notebook"));
+//
+//        set.remove("Cd");
+//
+//        set.removeIf(x -> x.charAt(0) == 'T');
+//
+//        for (String p : set) {
+//            System.out.println(p);
+//        }
 
-        set.add("Tv");
-        set.add("Notebook");
-        set.add("Tablet");
-        set.add("Sofá");
-        set.add("Cd");
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 
-        System.out.println(set.contains("Notebook"));
-
-        set.remove("Cd");
-
-        set.removeIf(x -> x.charAt(0) == 'T');
-
-        for (String p : set) {
-            System.out.println(p);
-        }
+        //União
+        Set<Integer> union = new TreeSet<>(a);
+        union.addAll(b);
+        System.out.println(union);
 
     }
 }
