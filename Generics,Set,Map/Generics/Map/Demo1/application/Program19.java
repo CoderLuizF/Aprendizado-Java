5caedd17bc1a64848bd3db85260b405dd027750b
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-23 10:27:39 -0300

    Map para armazenamento de cookies

---
+++ Generics,Set,Map/Generics/Map/Demo1/application/Program19.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program19 {
+
+    public static void main(String[] args) {
+
+        Map<String, String> cookies = new TreeMap<>();
+    }
+}
