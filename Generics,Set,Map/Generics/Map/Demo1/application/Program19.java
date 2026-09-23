3b82ba12b9ee965cdfb05a2a07d7172fccafd8ac
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-23 10:31:27 -0300

    Imprimir o map com o for() passando o cookies.keySet() onde vai retornar um Set<Key> com as chaves do Map<>

--- Generics,Set,Map/Generics/Map/Demo1/application/Program19.java
+++ Generics,Set,Map/Generics/Map/Demo1/application/Program19.java
@@ -10,5 +10,13 @@ public class Program19 {
         Map<String, String> cookies = new TreeMap<>();
 
         cookies.put("username", "Luiz");
+        cookies.put("email", "luiz@gmail.com");
+        cookies.put("phone", "991234567");
+
+        System.out.println("ALL COOKIES: ");
+        for(String key : cookies.keySet()) {
+
+        }
+
     }
 }
