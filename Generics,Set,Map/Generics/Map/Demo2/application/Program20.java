866f69ec55357894301c3e773bddf5bc141be6ca
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-25 09:33:01 -0300

    Mudando o nome do map, instanciando produtos e colocando dentro do Map<>

--- Generics,Set,Map/Generics/Map/Demo2/application/Program20.java
+++ Generics,Set,Map/Generics/Map/Demo2/application/Program20.java
@@ -9,8 +9,15 @@ public class Program20 {
 
     public static void main(String[] args) {
 
-        Map<ProdEx, Double> cookies = new HashMap<>();
+        Map<ProdEx, Double> stock = new HashMap<>();
 
+        ProdEx p1 = new ProdEx("Tv", 900.0);
+        ProdEx p2 = new ProdEx("Notebook", 1200.0);
+        ProdEx p3 = new ProdEx("Tablet", 400.0);
+
+        cookies.put(p1, 10000.0);
+        cookies.put(p2, 20000.0);
+        cookies.put(p3, 15000.0);
 
     }
 }
