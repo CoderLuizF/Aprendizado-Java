7f1da72a025fdc7dbd959e8355b4f8ca8cf5d37e
Luiz Felipe <zzzezizzz@gmail.com>
2026-09-25 09:28:48 -0300

    Map<> dentro do program

--- Generics,Set,Map/Generics/Map/Demo2/application/Program20.java
+++ Generics,Set,Map/Generics/Map/Demo2/application/Program20.java
@@ -1,11 +1,12 @@
 package application;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Program20 {
 
     public static void main(String[] args) {
 
-
+        Map<String, Double> cookies = new HashMap<>();
     }
 }
